263a7904e6e1bdcd6eb3063df99222135bd55a94
Hoff Silva <hoff.silva@castgroup.com.br>
2018-01-10 16:53:52 -0200

    Adicionados loop e timer no programa.

--- Lesson4/OpenWebBrowser.py
+++ Lesson4/OpenWebBrowser.py
@@ -1,2 +1,6 @@
 import webbrowser
-webbrowser.open("https://www.youtube.com/embed/kak8PEl_v1I")
+import time
+
+for vez in range(0, 3):
+    time.sleep(2)
+    webbrowser.open("https://www.youtube.com/embed/kak8PEl_v1I")
