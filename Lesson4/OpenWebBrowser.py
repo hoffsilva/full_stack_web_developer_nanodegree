588394482c4763f203d3face47fd8163e3d3ca64
Hoff Silva <hoff.henry@gmail.com>
2018-01-13 17:04:42 -0200

    Implementando o rename files.

--- Lesson4/OpenWebBrowser.py
+++ Lesson4/OpenWebBrowser.py
@@ -1,6 +1,7 @@
 import webbrowser
 import time
 
+print("The program was started on " + time.ctime())
 for vez in range(0, 3):
     time.sleep(2)
     webbrowser.open("https://www.youtube.com/embed/kak8PEl_v1I")
---
+++ Lesson4/rename_file.py
@@ -0,0 +1,3 @@
+import os
+listFiles = os.listdir("/Users/hoffhenrypereiradasilva/Google Drive/Nanodegree Full Stack Web Developer/full_stack_web_developer_nanodegree/Lesson4/prank")
+print(listFiles)
